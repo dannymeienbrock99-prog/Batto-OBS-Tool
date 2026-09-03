9b8fa5ba9eb545f5808bb500b6a8f0f7e5200502
Crazy_Batto <dannymeienbrock99@gmail.com>
2026-09-04 00:37:23 +0200

    fix: enrich hardware before app startup

---
+++ desktop/src/main-v2.cjs
@@ -0,0 +1,11 @@
+"use strict";
+
+const hardwareApi = require("./services/hardware.cjs");
+const { enrichHardware } = require("./services/hardware-enrichment-v2.cjs");
+
+const collectBaseHardware = hardwareApi.collectHardware.bind(hardwareApi);
+hardwareApi.collectHardware = async function collectEnrichedHardware() {
+  return enrichHardware(await collectBaseHardware());
+};
+
+require("./main.cjs");
