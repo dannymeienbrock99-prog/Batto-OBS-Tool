e195b4e5fe743a49d103367208e0673cff24ae12
Crazy_Batto <dannymeienbrock99@gmail.com>
2026-09-04 01:13:37 +0200

    fix: remove hardware diagnostics from 2.1 entrypoint

--- desktop/src/main-v2.cjs
+++ desktop/src/main-v2.cjs
@@ -1,13 +1,5 @@
 "use strict";
 
-const hardwareApi = require("./services/hardware.cjs");
-const { enrichHardware } = require("./services/hardware-enrichment-v2.cjs");
-
-const collectBaseHardware = hardwareApi.collectHardware.bind(hardwareApi);
-hardwareApi.collectHardware = async function collectEnrichedHardware() {
-  return enrichHardware(await collectBaseHardware());
-};
-
 require("./main.cjs");
 require("./stream-overlay-bootstrap.cjs");
 require("./deck-bootstrap.cjs");
