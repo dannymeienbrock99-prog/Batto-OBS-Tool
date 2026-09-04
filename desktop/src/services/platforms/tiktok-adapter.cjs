a890dc0e5e06b966bfd26afa76c09a4f7e94b57a
Crazy_Batto <dannymeienbrock99@gmail.com>
2026-09-04 06:57:57 +0200

    test: keep connector factory injection compatible

--- desktop/src/services/platforms/tiktok-adapter.cjs
+++ desktop/src/services/platforms/tiktok-adapter.cjs
@@ -3,8 +3,14 @@
 const { TikTokHybridAdapter, normalizeTikFinityPayload } = require("./tiktok-hybrid-adapter.cjs");
 const { isOfflineError } = require("./tiktok-direct-adapter.cjs");
 
+class TikTokAdapter extends TikTokHybridAdapter {
+  constructor(options = {}) {
+    super({ ...options, directOptions: options.directOptions || options });
+  }
+}
+
 module.exports = {
-  TikTokAdapter: TikTokHybridAdapter,
+  TikTokAdapter,
   TikTokHybridAdapter,
   normalizeTikFinityPayload,
   isOfflineError
